3957291e87dbae2feb27415587341a5d33f63abb
Matheus Henrique Sobrinho <matheusobrinho2k20@gmail.com>
2026-02-10 18:04:33 -0300

    ui: implement timer component.

--- src/template/Main/index.tsx
+++ src/template/Main/index.tsx
@@ -2,6 +2,7 @@
 import { Container } from '../../components/Container';
 import { Logo } from '../../components/Logo';
 import { StatusDisplay } from '../../components/StatusDisplay';
+import { Timer } from '../../components/Timer';
 
 export function Main() {
   return (
@@ -11,6 +12,7 @@ export function Main() {
       </Container>
       <Container>
         <StatusDisplay />
+        <Timer />
       </Container>
     </>
   );
