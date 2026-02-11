4ffddf8416f54cd1e2e4f381158e4f0e25e42fbc
Matheus Henrique Sobrinho <matheusobrinho2k20@gmail.com>
2026-02-11 15:11:57 -0300

    feat: implement count down logic

--- src/provider/TimerProvider/index.tsx
+++ src/provider/TimerProvider/index.tsx
@@ -16,7 +16,27 @@ export function TimerContextProvider({ children }: TimerContextProviderProps) {
 
   useEffect(() => {
     console.log(state);
-  }, [state]);
+
+    // Count Down logic
+    if (!state.isActive) return;
+
+    const interval = window.setInterval(() => {
+      setState((prevState) => {
+        if (!prevState.isActive) return prevState;
+
+        const currentSeconds = Math.max(prevState.secondsRemaining - 1, 0);
+
+        return {
+          ...prevState,
+          secondsRemaining: currentSeconds,
+          isActive: currentSeconds === 0 ? false : prevState.isActive,
+        };
+      });
+    }, 1000);
+
+    return () => window.clearInterval(interval);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state.isActive, setState]);
 
   return (
     <TimerContext.Provider value={{ state, setState }}>{children}</TimerContext.Provider>
