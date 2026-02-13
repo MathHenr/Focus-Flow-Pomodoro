bb9c888b0bc17bb23aedd2e62b9c2578b0c1d9e8
Matheus Henrique Sobrinho <matheusobrinho2k20@gmail.com>
2026-02-13 13:08:42 -0300

    feat: add popover radix

--- src/components/SettingsButton/index.tsx
+++ src/components/SettingsButton/index.tsx
@@ -1,7 +1,21 @@
+// import radix component
+import { Popover } from 'radix-ui';
+
 type SettingsButtonProps = {
   children: React.ReactNode;
 };
 
 export function SettingsButton({ children }: SettingsButtonProps) {
-  return <>{children}</>;
+  return (
+    <Popover.Root>
+      <Popover.Trigger>{children}</Popover.Trigger>
+      <Popover.Portal>
+        <Popover.Content>
+          <>
+            <h1>Popover</h1>
+          </>
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
+  );
 }
